e6176660711d32284d0aeaff2dceea62bfb94ac9
Hin <hunguet1471994@gmail.com>
2017-12-22 17:49:29 +0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React,{Component} from 'react';
+import '../App.css';
+import {connect} from 'react-redux';
+import {addReminder} from "../action/index";
+// import {bindActionCreators} from 'redux';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            text : ''
+        }
+    }
+    addReminder() {
+        this.props.addReminder(this.state.text);
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="title">
+                    Reminder Pro
+                </div>
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input type="text"
+                               className="form-control"
+                            placeholder="i have to ..."
+                               onChange={event => this.setState({text : event.target.value})}
+                        />
+                    </div>
+                    <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={() => this.addReminder()}
+                    >
+                        Add Reminder
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({addReminder},dispatch);
+}
+export default connect(null,{addReminder})(App);
